a675c6034fd0d613253240b93e2b21c796c56f89
postcert <postcert@gmail.com>
2023-11-04 19:16:33 -0700

    Extract provider from module to allow iteration.

--- proxmox-vm/examples/main.tf
+++ proxmox-vm/examples/main.tf
@@ -2,12 +2,28 @@
 # export PM_USER="someuser@pam"
 # export PM_PASS="somepassword"
 
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "https://192.168.1.10:8006/api2/json"
+}
+
 module "vm" {
   source = "../"
 
-  name        = "test-vm"
-  net_address = "192.168.5.5"
+  name         = "test-vm"
+  net_address  = "192.168.5.5"
   proxmox_node = "post7000"
 
   password = "password"
+
+  providers = {
+    proxmox = proxmox
+  }
 }
--- proxmox-vm/main.tf
+++ proxmox-vm/main.tf
@@ -1,12 +1,7 @@
-
 terraform {
   required_providers {
     proxmox = {
       source = "telmate/proxmox"
     }
   }
-}
-
-provider "proxmox" {
-  pm_api_url = var.proxmox_api_url
 }
